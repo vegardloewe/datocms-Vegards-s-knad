f236b1e712c85ec7f02bc76de31e039448ca803f
vegardloewe <62800747+vegardloewe@users.noreply.github.com>
2020-03-31 16:06:44 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 module.exports = {
   siteMetadata: {
-    title: `Creative Portfolio`,
+    title: `Vegards søknad`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
